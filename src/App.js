ffbcef33dfd7b12a246b268e287f433525f72547
MSamiDev <mohammadsami@duck.com>
2023-03-26 08:33:28 +0530

    Add: nasa map for ReasQmate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {  Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import NasaMap from "./NasaMap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [eventData, setEventData] = useState([]);
+
+	useEffect(() => {
+		const getNasaData = async () => {
+			const res = await axios.get(
+				"https://eonet.gsfc.nasa.gov/api/v2.1/events?limit=100"
+			);
+			// console.log(res.data.events);
+			let temp = [];
+			temp = [];
+			for (let i = 0; i < res.data.events.length; i++) {
+				temp.push({
+					title: res.data.events[i].title,
+					longitude: res.data.events[i].geometries[0].coordinates[0],
+					latitude: res.data.events[i].geometries[0].coordinates[1],
+					categories: res.data.events[i].categories[0].title,
+				});
+			}
+			setEventData([{}]);
+			setEventData(temp);
+			temp = [];
+		};
+
+		getNasaData();
+	}, []);
+	return (
+		<>
+			<Routes>
+				<Route
+					path="/nasamap"
+					element={<NasaMap eventData={eventData} />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ src/NasaMap.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./maps.css";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+import fire from "./images/icons/fire.png";
+import iceberg from "./images/icons/iceberg.png";
+import thunder from "./images/icons/thunder.png";
+import volcano from "./images/icons/volcano.png";
+
+export default function NasaMap({ eventData }) {
+
+  console.log("naana mao ", eventData);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC1mpaHajUPWU696t2u2xboKThZC-lRnnA",
+  });
+  const markers = eventData.map((ev, index) => {
+    return (
+      <Marker
+        key={index}
+        position={{
+          lat: ev.latitude,
+          lng: ev.longitude,
+        }}
+        icon={{
+          url:
+            ev.categories === "Severe Storms"
+              ? thunder
+              : ev.categories === "Volcanoes"
+              ? volcano
+              : ev.categories === "Sea and Lake Ice"
+              ? iceberg
+              : ev.categories === "Wildfires"
+              ? fire
+              : fire,
+        }}
+      />
+    );
+  });
+
+  const center2 = useMemo(() => ({ lat: 28.677592, lng: 77.2913126 }), []);
+  if (!isLoaded) return <div>Loading...</div>;
+  return (
+    <>
+      <GoogleMap
+        zoom={10}
+        center={center2}
+        mapContainerClassName="map-container"
+        ClassName="map-container"
+      >
+        {markers}
+      </GoogleMap>
+    </>
+  );
+}
+
+
